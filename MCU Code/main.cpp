68c9dace85c2762134067f3b06f0a92f8c2c6564
ZButcher97 <37154934+ZButcher97@users.noreply.github.com>
2018-03-14 22:01:07 +0000

    Updated C++ file

--- MCU Code/main.cpp
+++ MCU Code/main.cpp
@@ -1,60 +1,76 @@
 #include "mbed.h"
 
-SPI spi(PA_7, PA_6, PA_5);      // Ordered as: mosi, miso, sclk could use forth parameter ssel
-DigitalOut cs(PC_6);            // Chip Select for Basic Outputs to illuminate Onboard FPGA DEO nano LEDs CN7 pin 1
+//Pin assignments
+SPI SPI_Master (PA_7,PA_6,PA_5); 								//SPI1 MOSI, MISO, SCLK - PA_7, PA_6, PA_5
+BusOut SPI_CS(PF_13, PF_14);										//
+DigitalOut GreenLED(LED1);											//
+DigitalIn Button(USER_BUTTON);									//
+DigitalIn DO_NOT_USE(PB_12);    								// MAKE PB_12 (D19) an INPUT do NOT make an OUTPUT under any circumstances !!!!! ************* !!!!!!!!!!!
+																								// This Pin is connected to the 5VDC from the FPGA card and an INPUT is 5V Tolerant
+//Definitions																				
+#define SPI_CS_Character 01;										//CS address for the display character slave module 
+#define SPI_CS_Mode 10;													//CS address for the display mode slave module
+#define SPI_CS_Deselect 11;											//CS deselect word
+#define SPI_Data_None 0;												//SPI send no data word
 
-int32_t SendReadSPI(int d);    //Read 4 Sliding switches on FPGA (Simulating OPTO-Switches from Motor(s)
-void PrintVal(int sw_val);
+//Functions 
+int SPI_SendRead(int d, int CS);
 
-//NBB the following line for F429ZI !!!!
-DigitalIn DO_NOT_USE(PB_12);    // MAKE PB_12 (D19) an INPUT do NOT make an OUTPUT under any circumstances !!!!! ************* !!!!!!!!!!!
-                                // This Pin is connected to the 5VDC from the FPGA card and an INPUT is 5V Tolerant
-BusIn BUTTONs (USER_BUTTON, D4, D3);
-BusOut MCU_LEDs (D7, D6, D5);
-
-//Ticker ticktock;
-  
-int main() {
-    cs = 1;                     // Chip must be deselected, Chip Select is active LOW
-    spi.format(8,0);            // Setup the DATA frame SPI for 16 bit wide word, Clock Polarity 0 and Clock Phase 0 (0)
-    spi.frequency(1000000);     // 1MHz clock rate
-
-	
-		printf("TEST\n\r");
-	//This sets up an 8 bit value to send over SPI
-  int LED_Val = 0b00000000;
-	//Value read from MISO SPI
-	int Data_Read = 0;
-	//Main program loop
-	while(true)                 //Loop forever Knight Rider Display on FPGA
-    {		
-			//Sets the MSB 3 bits to the Button inputs
-			LED_Val = BUTTONs << 5;
-			//sends data and reads MISO
-			Data_Read = SendReadSPI(LED_Val);
-			//sets the MCU leds to show what buttons are being pressed
-			MCU_LEDs = BUTTONs;
-			//prints the MISO data read and outputs to PuTTY
-			PrintVal(Data_Read);
-			wait_ms(20);
-    }
-}
-
-//Function to read back the state of the switches
-int SendReadSPI(int d){
-    int sw_val = 0;
-    cs = 0;				//Select the device by seting chip select LOW
-    spi.write(0);	//Command
-    sw_val = spi.write(d); // & 0x0F;  Mask to read back lower 4bit nibble
-    cs = 1 ;			//De-select the device by seting chip select HIGH
-    return sw_val;    
-}
-
-void PrintVal(int sw_val)
+//Main code
+int main()
 {
-	    if (sw_val&(1<<0)){ printf("Switch 0 :"); }
-    if (sw_val&(1<<1)){ printf("Switch 1 :"); }
-    if (sw_val&(1<<2)){ printf("Switch 2 :"); }
-    if (sw_val&(1<<3)){ printf("Switch 3 :"); }
-    if (sw_val>0)     { printf("\r\n");       }
+		//This sets up the SPI when starting up the MCU
+		
+		printf("Initalising.../n/r");
+			
+		SPI_CS = SPI_CS_Deselect;                   // Chip must be deselected, Chip Select is active LOW
+		spi.format(8,0);            								// Setup the DATA frame SPI for 16 bit wide word, Clock Polarity 0 and Clock Phase 0 (0)
+		spi.frequency(1000000);     								// 1MHz clock rate
+
+		//Local Variables
+		int SPI_test_send_data = 0b11111111;
+		int SPI_recieved_data = 0;
+	
+		//This is the rest of the start up code.
+		GreenLED = 1;
+		
+		printf("Initalised/n/r");
+		printf("Starting main loop./n/r");
+	
+		//This is the main loop that happens after the initlisation has happend on start up		
+		while(1)
+		{
+				if(Button == 1)
+				{
+						SPI_test_send_data = 0b11111111;
+						//LED and printf are for debugging
+						GreenLED = 1;
+						printf("Button Pressed/n/r");
+				} else {
+						SPI_test_send_data = 0b00000000;
+						GreenLED = 0;
+				}
+				
+				SPI_recieved_data(SPI_test_send_data, 00);
+				wait_ms(20);
+		}
+}
+
+//SPI function to send and recieve data from the selected slave module. Input Data(0 if you dont want to send anything) and the slave module CS address)
+int SPI_SendRead(int d, int CS)
+{
+		int SPI_Returned_Date = 0;
+		SPI_CS = CS;																//Select the device by seting chip select LOW
+		SPI_Master.write(0);												//Command
+		SPI_Returned_Date = SPI_Master.write(d); 		// & 0x0F;  Mask to read back lower 4bit nibble
+		SPI_CS = SPI_CS_Deselect;										//De-select the device by seting chip select HIGH
+		return SPI_Returned_Date;    								//Returns recieved data from selected slave module
+}
+
+int SPI_ConfigReg(int Reset, int Samp)
+{
+	int RegValue = 0;
+	int Instruction = 0;
+	
+	
 }
